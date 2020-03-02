a1a7c8ec90d35b48468719885d633d48c45dd587
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-03-02 08:38:42 +0300

    added functions for the menu

--- js/scripts.js
+++ js/scripts.js
@@ -74,35 +74,3 @@ function down(min) {
     document.getElementById("myNumber").value = min;
   }
 }
-
-function validateForm() {
-
-var deliveryOption = document.forms("delivery"]['yes']["no"].value;
-if deliveryOption == yes
-var x=document.forms["contactUs"]["name"].value;
-if (x==null || x=="")
-  {
-  alert("Name must be filled out");
-  return false;
-  }
-
-var y=document.forms["contactUs"]["email"].value;
-if (y==null || y=="") {
-  alert("Email must be filled out");
-  return false;
-  }
-
-  var y=document.forms["contactUs"]["phone"].value;
-  if (y==null || y=="") {
-    alert("Phone number must be filled out");
-    return false;
-    }
-
-  var z=document.forms["contactUs"]["subject"].value;
-  if (z==null || z=="") {
-    alert("Please type your adress");
-  }
-    else {
-      alert ("Your order will be delivered to your adress");
-    }
-}
