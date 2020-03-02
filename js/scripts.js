52873cc96570ff3919a7b05254124a0e59554f25
Mohamed Mohamed <demahom93@yahoo.co.uk>
2020-03-02 08:53:57 +0300

    created function to alert on order completion

--- js/scripts.js
+++ js/scripts.js
@@ -74,3 +74,33 @@ function down(min) {
     document.getElementById("myNumber").value = min;
   }
 }
+function validateForm() {
+
+
+var x=document.forms["contactUs"]["name"].value;
+if (x==null || x=="")
+  {
+  alert("Name must be filled out");
+  return false;
+  }
+
+var y=document.forms["contactUs"]["email"].value;
+if (y==null || y=="") {
+  alert("Email must be filled out");
+  return false;
+  }
+
+  var y=document.forms["contactUs"]["phone"].value;
+  if (y==null || y=="") {
+    alert("Phone number must be filled out");
+    return false;
+    }
+
+  var z=document.forms["contactUs"]["subject"].value;
+  if (z==null || z=="") {
+    alert("Please type your adress");
+  }
+    else {
+      alert ("Your order will be delivered to your adress");
+    }
+}
